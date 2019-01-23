5c2fb7b7385321661b699125294f87d3b06c7f75
colemab5 <ben.coleman5@mail.dcu.ie>
2019-01-23 20:25:22 +0000

    fixed uploading issues - stored in uploads dir

--- src/app/app/routes/index.js
+++ src/app/app/routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var formidable = require('formidable');
 var fs = require('fs');
 var util = require('util');
+var path = require('path');
+var bodyParser = require('body-parser');
 var router = express.Router();
 
 /* GET home page. */
@@ -10,21 +12,27 @@ router.get('/', function(req, res, next) {
 });
 
 /* Handle uploaded file */
-router.post('/myfile', function (req, res){
+router.post('/', function (req, res){
     
     var form = new formidable.IncomingForm();
 
+	form.uploadDir = "../uploads/";
     form.parse(req);
 
-    form.uploadDir = "../uploads/";
+    console.log('hello');
 
     form.on('fileBegin', function (name, file){
-        file.path = __dirname + '/uploads/' + file.name;
+    	saveDir = path.join(__dirname, '../uploads/')
+        file.path = saveDir + file.name;
     });
+
+    console.log('helpme');
 
     form.on('file', function (name, file){
         console.log('Uploaded file', name, file);
     });
+
+    console.log('please');
 
     return res.status(200).json({});
 });

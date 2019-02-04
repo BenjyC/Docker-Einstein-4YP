33104dcaafea06afdfa9a4aeb7d68c48eb6ace1b
colemab5 <ben.coleman5@mail.dcu.ie>
2019-02-04 20:31:34 +0000

    Added status/content unit tests

--- src/app/app/test/test-feedback.js
+++
@@ -1,9 +0,0 @@
-var expect  = require('chai').expect;
-var request = require('request');
-
-it('Feedback content', function(done) {
-    request('http://localhost:3000/feedback' , function(error, res, body) {
-        expect(body).to.exist;
-        done();
-    });
-});
--- src/app/app/test/test-mainpage.js
+++
@@ -1,9 +0,0 @@
-var expect  = require('chai').expect;
-var request = require('request');
-
-it('Main page content', function(done) {
-    request('http://localhost:3000' , function(error, res, body) {
-        expect(body).to.exist;
-        done();
-    });
-});

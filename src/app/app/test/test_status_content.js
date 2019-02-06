1dd902f529d1c8499f59597b2d6552b3913ec9d7
colemab5 <ben.coleman5@mail.dcu.ie>
2019-02-06 17:22:07 +0000

    Edited status_content unit test to meet conventions

--- src/app/app/test/test_status_content.js
+++ src/app/app/test/test_status_content.js
@@ -4,14 +4,14 @@ var request = require('request');
 
 describe('Status and Content', function() {
 	describe('Upload page', function() {
-		it('status', function(done){
+		it('checks status', function(done){
 			request('http://localhost:3000' , function(err, res, body) {
         		expect(res.statusCode).to.equal(200);
         		done();
        		});
 		});
 
-		it('content', function(done) {
+		it('checks content', function(done) {
 			request('http://localhost:3000' , function(error, res, body) {
         		expect(body).to.contain("Docker Einstein upload page");
         		done();
@@ -21,14 +21,14 @@ describe('Status and Content', function() {
 	});
 
 	describe('Feedback page', function() {
-		it('status', function(done){
+		it('checks status', function(done){
 			request('http://localhost:3000/feedback', function(err, res ,body){
 				expect(res.statusCode).to.equal(200);
 				done();
 			});
 		});
 
-		it('content', function(done){
+		it('checks content', function(done){
 			request('http://localhost:3000/feedback', function(err, res ,body){
 				expect(body).to.contain('Upload feedback page');
 				done();
